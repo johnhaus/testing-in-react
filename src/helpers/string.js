7801bfef5eb15ce4ea26ceaed376fccd3a6bde51
johnhaus <john.haus@outlook.com>
2019-04-22 17:34:48 -0300

    refactored for readability

--- src/helpers/string.js
+++ src/helpers/string.js
@@ -1,10 +1,6 @@
 export function capitalize(word) {
-  if (word === null) {
-    return null;
+  if (!word) {
+    return word;
   }
-  if (word === "") {
-    return "";
-  }
-  return word[0].toUpperCase() +
-    word.substr(1).toLowerCase();
+  return `${word[0].toUpperCase()}${word.substr(1).toLowerCase()}`;
 }
